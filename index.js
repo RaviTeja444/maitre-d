98841a66e7e8a63eaac84cc11be6a8377cfd153e
RaviTeja444 <pagidojuraviteja2@gmail.com>
2020-02-17 17:45:19 -0600

    adding toFixed()

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function functioncalc() {
     const total = document.getElementById('total').value;
     const rate = document.getElementById('rate').value;
     const tip = rate * total / 100;
-    const tax = 55 * total / 1000;
+    const tax = (55 * total / 1000).toFixed(4);
     const grandtotal = Number(total) + tip + tax;
     document.getElementById('tip').value = '$'+tip;
     document.getElementById('tax').value = '$'+tax;
