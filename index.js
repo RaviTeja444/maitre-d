829febfdcc1cb5ef426ad644cab0a61ee8a96e8d
RaviTeja444 <pagidojuraviteja2@gmail.com>
2020-02-17 16:50:37 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+document.addEventListener('click', event => {
+    if (event.target && event.target.id === "btn") { functioncalc() }
+})
+
+function functioncalc() {
+    const total = document.getElementById('total').value;
+    const rate = document.getElementById('rate').value;
+    const tip = rate * total / 100;
+    const tax = 55 * total / 1000;
+    const grandtotal = Number(total) + tip + tax;
+    document.getElementById('tip').value = '$'+tip;
+    document.getElementById('tax').value = '$'+tax;
+    document.getElementById('grandtotal').value = '$'+grandtotal;
+}
